46b540ae52b47a3611ff223fe51f3fcfa5f837d8
shundahoy <shundahoy@gmail.com>
2022-09-27 21:36:46 +0900

    first

---
+++ src/book_log.php
@@ -0,0 +1,12 @@
+<?php 
+    // echo "書籍名 : 銀河鉄道の夜". PHP_EOL;
+    // echo "著者名 : 宮沢健司". PHP_EOL;
+    echo "読書ログを登録してください" . PHP_EOL;
+    echo "書籍名";
+    $title = fgets(STDIN);
+    echo "登録完了、表示します" . PHP_EOL;
+
+    echo "読書名:" . $title . PHP_EOL;
+
+
+?>
---
+++ src/tmp/echo.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "Hi" . PHP_EOL;
+
+?>
